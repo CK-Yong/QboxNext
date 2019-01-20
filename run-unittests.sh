d28341d651ef056c011909ecac5ddbfb2d73908b
skwas <skwas@skwas.net>
2019-01-20 11:59:00 +0100

    Fix build hangs

--- run-unittests.sh
+++ run-unittests.sh
@@ -2,11 +2,11 @@
 
 # https://confluence.atlassian.com/bitbucket/test-reporting-in-pipelines-939708543.html
 
-find . -type f -name '*Tests.csproj' -exec dotnet test --no-build --logger 'trx;LogFileName=../test-results/results.trx' --filter TestCategory!='Integration' "{}" \;
+find . -type f -name '*Tests.csproj' -exec dotnet test --logger 'trx;LogFileName=../test-results/results.trx' --filter TestCategory!='Integration' "{}" \;
 
 # Compile reports
 
-export PATH="$PATH:/root/.dotnet/tools"
 dotnet tool install -g trx2junit
+export PATH="$PATH:/root/.dotnet/tools"
 
 trx2junit **/test-results/*.trx
