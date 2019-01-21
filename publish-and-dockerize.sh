03895add9cae1d7de182f466596031917acc580d
Chee Kent Yong <cheekentyong@gmail.com>
2019-01-22 00:17:55 +0100

    Swapped sequence of publishing port parameters, was causing trouble on windows.

--- publish-and-dockerize.sh
+++ publish-and-dockerize.sh
@@ -3,5 +3,5 @@ dotnet publish QboxNext.Qserver.sln -c Release
 docker build ./QboxNext.Qserver/. -t "dotnetcore-minimal_qserver"
 docker build ./QboxNext.Qservice/. -t "dotnetcore-minimal_qservice"
 
-docker run dotnetcore-minimal_qserver -p 80:5000 -v qboxnextdata:/var/qboxnextdata
-docker run dotnetcore-minimal_qservice -p 5001:5001 -v qboxnextdata:/var/qboxnextdata
+docker run -p 80:5000 -v qboxnextdata:/var/qboxnextdata dotnetcore-minimal_qserver 
+docker run -p 5001:5001 -v qboxnextdata:/var/qboxnextdata dotnetcore-minimal_qservice 
